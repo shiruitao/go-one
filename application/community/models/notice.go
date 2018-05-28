6b343085d23476d66964bff246bd8e9936d89a0a
shitime <1257201113@qq.com>
2018-05-28 17:05:36 +0800

    HJJC

--- application/community/models/notice.go
+++ application/community/models/notice.go
@@ -36,6 +36,6 @@ func (*NoticeServiceProvider) Get() (*[]Notice, error) {
 	var n []Notice
 
 	o := orm.NewOrm()
-	_, err := o.Raw("SELECT * FROM notice").QueryRows(&n)
+	_, err := o.Raw("SELECT * FROM notice ORDER BY id DESC").QueryRows(&n)
 	return &n, err
 }
