6bff50f58a0d3da95ddac32b284023289b74a85d
shitime <1257201113@qq.com>
2018-05-30 21:33:44 +0800

    hjjc

--- application/hjjc/controllers/company.go
+++ application/hjjc/controllers/company.go
@@ -18,19 +18,12 @@ func (this *CompanyController) Get() {
 		Latitude  float64 `json:"latitude"`
 	}
 
-	pos := []position{}
-
 	info, err := models.CompanyService.Get()
 	if err != nil {
 		log.Logger.Error("ErrMysql", err)
 		this.Data["json"] = map[string]interface{}{common.RespKeyStatus: common.ErrMysqlQuery}
 	} else {
-		for i := 0; i <100; i++ {
-			pos[i].Latitude = info[i].WD
-			pos[i].Longitude = info[i].JD
-			log.Logger.Info("", info[i])
-		}
-		this.Data["json"] = map[string]interface{}{common.RespKeyStatus: common.ErrSucceed, "data": info, "position": pos}
+		this.Data["json"] = map[string]interface{}{common.RespKeyStatus: common.ErrSucceed, "data": info}
 	}
 
 	this.ServeJSON()
