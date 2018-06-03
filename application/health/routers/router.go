13f1a643de4cdf2ebf5236a8ebc42731b09586d2
shitime <1257201113@qq.com>
2018-06-03 22:18:53 +0800

    health

---
+++ application/health/routers/router.go
@@ -0,0 +1,16 @@
+package routers
+
+import (
+	"github.com/shiruitao/go-one/application/health/controllers"
+
+	"github.com/astaxie/beego"
+)
+
+func init() {
+	beego.Router("/health/user/login", &controllers.UserController{}, "post:CreateUser")
+
+	beego.Router("/health/record/get", &controllers.RecordController{}, "post:Get")
+
+	beego.Router("/health/user/test", &controllers.TestController{}, "post:Get")
+
+}
